a0ea650e832f6038833f336b9bd3d8f9a6cad5da
Nikita <ptznikko@gmail.com>
2021-10-06 22:11:50 +0300

    changing env var name

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     development: {
         client: "postgresql",
-        connection: process.env.PG_CONNECTION_STRING,
+        connection: process.env.DATABASE_URL,
         migrations: {
             tableName: "knex_migrations"
         }
@@ -11,7 +11,7 @@ module.exports = {
 
     production: {
         client: "postgresql",
-        connection: process.env.PG_CONNECTION_STRING,
+        connection: process.env.DATABASE_URL,
         migrations: {
             tableName: "knex_migrations"
         }
